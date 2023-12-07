7aa49c2d196ab5c59bc4eb9c353b4f4d770ed3be
Rafael Paroni <rafael.paronis.oficial@gmail.com>
2023-12-07 16:28:31 -0300

    Adicionando função de detectar linguagem padrão do navegador do usuário e do local Storage do usuário

--- src/index.js
+++ src/index.js
@@ -6,6 +6,35 @@ import NavbarComponent from './components/Navbar/Navbar';
 import FooterComponent from './components/Footer/Footer';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
+
+
+function setLanguage(){
+ 
+  if(window.localStorage.getItem('lang') === null || window.localStorage.getItem('lang') === ''){
+    if(window.navigator.language.includes('pt')){
+      window.location = '/pt/'
+      window.localStorage.setItem('lang', 'pt')
+    }else if(window.navigator.language.includes('en')){
+      window.location = '/en/'
+      window.localStorage.setItem('lang', 'en')
+    }else if(window.navigator.language.includes('es')){
+      window.location = '/es/'
+      window.localStorage.setItem('lang', 'es')
+    }else{
+      window.location = '/en/'
+      window.localStorage.setItem('lang', 'en')
+    }
+  }else{
+    if(!window.location.pathname.includes(window.localStorage.getItem('lang'))){
+      window.location = '/' + window.localStorage.getItem('lang') + '/'
+    }
+  }
+
+ 
+}
+setLanguage()
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
