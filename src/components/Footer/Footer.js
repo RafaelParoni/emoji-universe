ed11fc75beb1643e463d1a0de944cb6fc0a67927
Rafael Paroni <rafael.paronis.oficial@gmail.com>
2023-12-08 16:13:19 -0300

    adicionando Línguas no site, espanhol, inglês  e português

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,6 +7,84 @@ import { MdTranslate, MdDarkMode, MdLightMode } from "react-icons/md";
 
  function FooterComponent() {
 
+    function editLing(value){
+        console.log(value)
+        window.location.pathname = '/' + value + '/'
+        window.localStorage.setItem('lang', value)
+    }
+    setTimeout(function(){
+        document.getElementById('lang').value = window.localStorage.getItem('lang')
+    },)
+
+
+   var langs = {
+        pt: {
+            'Name': 'Emoji Universe',
+            'Description': 'feito com a intenção de facilitar para quem quer "copiar" os emojis para a área de transferência, podendo assim utilizá-los em locais',
+            'MySite': 'Meu site',
+            'share': 'compartilhar',
+            'OutherProjects': 'Outros projetos:',
+            'lang': 'Linguagem',
+            'credits': 'Créditos:',
+            'CarinhasAniamis': 'Carinhas e Aniamis',
+            'Pessoas': 'Pessoas',
+            'ComidasPlantas': 'Comidas  e plantas',
+            'ComemoraçõesObjetos': 'Comemorações  e Objetos ',
+            'TransporteLocais': 'Transporte  e locais',
+            'Símbolos': 'Símbolos',
+        },
+        en: {
+            'Name': 'Emoji Universe',
+            'Description': 'made with the intention of making it easier for those who want to "copy" the emojis to the clipboard, thus being able to use them in places',
+            'MySite': 'My personal website',
+            'share': 'share',
+            'OutherProjects': 'Other projects:',
+            'lang': 'Language',
+            'credits': 'Credits:',
+            'CarinhasAniamis': 'Faces and Aniamis',
+            'Pessoas': 'People',
+            'ComidasPlantas': 'Foods & Plants',
+            'ComemoraçõesObjetos': 'Commemorations and Objects',
+            'TransporteLocais': 'Transportation & Locations',
+            'Símbolos': 'Symbols',
+        },
+        es: {
+            'Name': 'Emoji Universe',
+            'Description': 'Hecho con la intención de facilitar a aquellos que quieran "copiar" los emojis al portapapeles, pudiendo así usarlos en lugares',
+            'MySite': 'Mi sitio web personal',
+            'share': 'Compartir',
+            'OutherProjects': 'Otros proyectos:',
+            'lang': 'Idioma',
+            'credits': 'Créditos:',
+            'CarinhasAniamis': 'Rostros y Aniamis',
+            'Pessoas': 'Gente',
+            'ComidasPlantas': 'Comidas  e plantas',
+            'ComemoraçõesObjetos': 'Alimentos y plantas',
+            'TransporteLocais': 'Transporte y Ubicaciones',
+            'Símbolos': 'Símbolos',
+        }
+   }
+
+   var lang = {}
+
+   async function setLang(){
+        var langSeletc = window.localStorage.getItem('lang')
+
+        switch(langSeletc){
+            case 'pt':
+                lang = langs.pt
+            break
+            case 'es':
+                lang = langs.es
+            break
+            default: 
+                lang = langs.en
+            break
+        }
+   }
+   setLang()
+
+
    function setTheme(theme){
         console.log(theme)
         window.localStorage.setItem('theme', theme)
@@ -45,11 +123,6 @@ import { MdTranslate, MdDarkMode, MdLightMode } from "react-icons/md";
             </div>
         </div>
         <div className='credits'>
-            <span className='select-lang-label'><MdTranslate/> Linguagem</span>
-            <select className='select-lang' name='select-lang'>
-                <option value="pt-br">Português(brasil)</option>
-                <option value="en-us">Inglês</option>
-                <option value="es">Espanhol</option>
             <div className='theme-div'>
                 <div>
                     <span><LuCat/> Themes:</span> <br/>
@@ -70,6 +143,11 @@ import { MdTranslate, MdDarkMode, MdLightMode } from "react-icons/md";
                 </div>
 
             </div>
+            <span className='select-lang-label'><MdTranslate/> {lang.lang}</span>
+            <select id='lang' onChange={(e)=> editLing(e.target.value)} className='select-lang' name='select-lang'>
+                <option value="pt">Português(brasil)</option>
+                <option value="en">English</option>
+                <option value="es">Español</option>
             </select>
             <h3><LuCrown/> {lang.credits}</h3>
             <p>Builder: <a href='https://www.instagram.com/rafaelparroni/'>Rafael Paroni</a></p>
