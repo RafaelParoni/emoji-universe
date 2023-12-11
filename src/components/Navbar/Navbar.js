c68c82d0bd9fe4abf7be2c40ff356b14c1c79d65
Rafael Paroni <rafael.paronis.oficial@gmail.com>
2023-12-11 17:30:24 -0300

    Atualizando Navbar para Inglês, espanhol  e português

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,6 +3,52 @@ import FavIcon from './../Icons/FavIcon512px.ico'
 import { LuGithub,LuInstagram,LuGlobe   } from "react-icons/lu";
 
 function NavbarComponent() {
+  var langs = {
+    pt: {
+        'CarinhasAniamis': 'Carinhas e Aniamis',
+        'Pessoas': 'Pessoas',
+        'ComidasPlantas': 'Comidas  e Natureza',
+        'ComemoraçõesObjetos': 'Comemorações  e Objetos ',
+        'TransporteLocais': 'Transporte  e locais',
+        'Símbolos': 'Símbolos',
+    },
+    en: {
+        'CarinhasAniamis': 'Faces and Aniamis',
+        'Pessoas': 'People',
+        'ComidasPlantas': 'Foods & Nature',
+        'ComemoraçõesObjetos': 'Commemorations and Objects',
+        'TransporteLocais': 'Transportation & Locations',
+        'Símbolos': 'Symbols',
+    },
+    es: {
+        'CarinhasAniamis': 'Rostros y Aniamis',
+        'Pessoas': 'Gente',
+        'ComidasPlantas': 'Alimentos y Naturaleza',
+        'ComemoraçõesObjetos': 'Conmemoraciones y Objetos',
+        'TransporteLocais': 'Transporte y Ubicaciones',
+        'Símbolos': 'Símbolos',
+    }
+}
+
+var lang = {}
+
+async function setLang(){
+    var langSeletc = window.localStorage.getItem('lang')
+
+    switch(langSeletc){
+        case 'pt':
+            lang = langs.pt
+        break
+        case 'es':
+            lang = langs.es
+        break
+        default: 
+            lang = langs.en
+        break
+    }
+}
+setLang()
+
 
   return (
     <nav>
@@ -11,12 +57,12 @@ function NavbarComponent() {
             <h1> EMOJI UNIVERSE</h1>
         </button>
         <div className='buttonsNav'>
-            <button onClick={()=>  window.location.hash = '#faces'}>Carinhas <br/> e Aniamis</button>
-            <button onClick={()=> window.location.hash = '#people'}>Pessoas</button>
-            <button onClick={()=> window.location.hash = '#food'}>Comidas <br/> e plantas</button>
-            <button onClick={()=> window.location.hash = '#celebrations'}>Comemorações <br/> e Objetos</button>
-            <button onClick={()=> window.location.hash = '#transport'}>Transporte <br/> e locais </button>
-            <button onClick={()=> window.location.hash =  '#symbols'}>Símbolos</button>
+            <button onClick={()=>  window.location.hash = '#faces'}>{lang.CarinhasAniamis}</button>
+            <button onClick={()=> window.location.hash = '#people'}>{lang.Pessoas}</button>
+            <button onClick={()=> window.location.hash = '#food'}>{lang.ComidasPlantas}</button>
+            <button onClick={()=> window.location.hash = '#celebrations'}>{lang.ComemoraçõesObjetos}</button>
+            <button onClick={()=> window.location.hash = '#transport'}>{lang.TransporteLocais} </button>
+            <button onClick={()=> window.location.hash =  '#symbols'}>{lang.Símbolos}</button>
         </div>
         <div id='socialBar' className='socialNav'>
             <button onClick={()=> window.open('https://github.com/RafaelParoni/')}><LuGithub size={30}/></button>
